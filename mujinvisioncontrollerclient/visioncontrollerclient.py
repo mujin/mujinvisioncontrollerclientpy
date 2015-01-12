2d83d6c8dda3ea534a63d970358af89fac397c3d
Huan Liu <liuhuanjim013@gmail.com>
2015-01-12 21:19:20 +0900

    added maxage param; cleaned up ignoreocclusion param

--- mujinvisioncontrollerclient/visioncontrollerclient.py
+++ mujinvisioncontrollerclient/visioncontrollerclient.py
@@ -11,6 +11,7 @@ log = logging.getLogger(__name__)
 # mujin imports
 from mujincontrollerclient import zmqclient
 
+
 class VisionControllerClient(object):
     """mujin vision controller client for bin picking task
     """
@@ -41,15 +42,14 @@ class VisionControllerClient(object):
         :param imagesubscriberConfigurationFilename: name of the config file for image subscribers, e.g. /home/controller/mujin/visioncontroller/mujindetection/imagesubscriber.json
         :param controllerclient: pointer to the BinpickingControllerClient that connects to the mujin controller we want the vision server to talk to
         """
-
         controllerusernamepass = '%s:%s'%(controllerclient.controllerusername, controllerclient.controllerpassword)
         command = {"command": "Initialize",
                    "detectorConfigurationFilename": detectorConfigurationFilename,
                    "imagesubscriberConfigurationFilename": imagesubscriberConfigurationFilename,
                    "mujinControllerIp": controllerclient.controllerIp,
                    "mujinControllerPort": controllerclient.controllerPort,
-                   "mujinControllerUsernamePass":controllerusernamepass,
-                   "robotControllerUri":controllerclient.robotControllerUri,
+                   "mujinControllerUsernamePass": controllerusernamepass,
+                   "robotControllerUri": controllerclient.robotControllerUri,
                    "binpickingTaskZmqPort": controllerclient.taskzmqport,
                    "binpickingTaskHeartbeatPort": controllerclient.taskheartbeatport,
                    "binpickingTaskHeartbeatTimeout": controllerclient.taskheartbeattimeout,
@@ -65,15 +65,16 @@ class VisionControllerClient(object):
         try:
             response = self._zmqclient.SendCommand(command)
             log.info('initialized server, took: %s seconds'%(response['computationtime']/1000.0))
-        except :
+        except:
             log.info(response)
         return response
 
-    def DetectObjects(self,regionname=None,cameranames=None, ignoreocclusion=None):
+    def DetectObjects(self, regionname=None, cameranames=None, ignoreocclusion=None, maxage=None):
         """detects objects
         :param regionname: name of the bin
         :param cameranames: a list of names of cameras to use for detection, if None, then use all cameras available
         :param ignoreocclusion: whether to skip occlusion check
+        :param maxage: max time difference in ms allowed between the current time and the timestamp of image used for detection, 0 means infinity
         :return: detected objects in world frame in a json dictionary, the translation info is in milimeter, e.g. {'objects': [{'name': 'target_0', 'translation_': [1,2,3], 'quat_': [1,0,0,0], 'confidence': 0.8}]}
         """
         log.info('Detecting objects...')
@@ -85,28 +86,32 @@ class VisionControllerClient(object):
         if cameranames is not None:
             command['cameranames'] = list(cameranames)
         if ignoreocclusion is not None:
-            command['ignoreocclusion'] = 1 if ignoreocclusion == True else 0
-        response=self._zmqclient.SendCommand(command)
+            command['ignoreocclusion'] = 1 if ignoreocclusion is True else 0
+        if maxage is not None:
+            command['maxage'] = maxage
+        response = self._zmqclient.SendCommand(command)
         try:
             log.info('detectd %d objects, took: %s seconds'%(len(response['objects']),response['computationtime']/1000.0))
-        except :
+        except:
             log.info(response)
         return response
 
-    def StartDetectionThread(self,regionname=None, cameranames=None,voxelsize=None, pointsize=None, ignoreocclusion=None):
+    def StartDetectionThread(self, regionname=None, cameranames=None, voxelsize=None, pointsize=None, ignoreocclusion=None, maxage=None):
         """starts detection thread to continuously detect objects. the vision server will send detection results directly to mujin controller.
         :param regionname: name of the bin
         :param cameranames: a list of names of cameras to use for detection, if None, then use all cameras available
         :param voxelsize: in meter
         :param pointsize: in meter
         :param ignoreocclusion: whether to skip occlusion check
+        :param maxage: max time difference in ms allowed between the current time and the timestamp of image used for detection, 0 means infinity
         :return: returns immediately once the call completes
         """
         log.info('Starting detection thread...')
         if regionname is None:
             regionname = self.regionname
         command = {"command": "StartDetectionLoop",
-                   "regionname":regionname}
+                   "regionname": regionname,
+                   }
         if cameranames is not None:
             command['cameranames'] = list(cameranames)
         if voxelsize is not None:
@@ -114,8 +119,10 @@ class VisionControllerClient(object):
         if pointsize is not None:
             command['pointsize'] = pointsize
         if ignoreocclusion is not None:
-            command['ignoreocclusion'] = 1 if ignoreocclusion == True else 0
-        response=self._zmqclient.SendCommand(command)
+            command['ignoreocclusion'] = 1 if ignoreocclusion is True else 0
+        if maxage is not None:
+            command['maxage'] = maxage
+        response = self._zmqclient.SendCommand(command)
         log.info(response)
         return response
 
@@ -124,11 +131,11 @@ class VisionControllerClient(object):
         """
         log.info('Stopping detection thread...')
         command = {"command": "StopDetectionLoop"}
-        response=self._zmqclient.SendCommand(command)
+        response = self._zmqclient.SendCommand(command)
         log.info(response)
         return response
 
-    def SendPointCloudObstacleToController(self,regionname=None,cameranames=None,detectedobjects=None,voxelsize=None, pointsize=None):
+    def SendPointCloudObstacleToController(self, regionname=None, cameranames=None, detectedobjects=None, voxelsize=None, pointsize=None):
         """Updates the point cloud obstacle with detected objects removed and sends it to mujin controller
         :param regionname: name of the region
         :param cameranames: a list of camera names to use for visualization, if None, then use all cameras available
@@ -156,10 +163,12 @@ class VisionControllerClient(object):
             log.info(response)
         return response
 
-    def DetectRegionTransform(self,regionname=None,cameranames=None):
+    def DetectRegionTransform(self, regionname=None, cameranames=None, ignoreocclusion=None, maxage=None):
         """Detects the transform of the region
         :param regionname: name of the region
         :param cameranames: a list of camera names to use for visualization, if None, then use all cameras available
+        :param ignoreocclusion: whether to skip occlusion check
+        :param maxage: max time difference in ms allowed between the current time and the timestamp of image used for detection, 0 means infinity
         """
         log.info('Detecting transform of region')
         if regionname is None:
@@ -169,6 +178,10 @@ class VisionControllerClient(object):
                    }
         if cameranames is not None:
             command['cameranames'] = list(cameranames)
+        if ignoreocclusion is not None:
+            command['ignoreocclusion'] = 1 if ignoreocclusion is True else 0
+        if maxage is not None:
+            command['maxage'] = maxage
         response = self._zmqclient.SendCommand(command)
         try:
             log.info('Detected region transform, took %s seconds'%(response['computationtime']/1000.0))
@@ -176,11 +189,13 @@ class VisionControllerClient(object):
             log.info(response)
         return response
 
-    def VisualizePointCloudOnController(self,regionname=None,cameranames=None,pointsize=None):
+    def VisualizePointCloudOnController(self, regionname=None, cameranames=None, pointsize=None, ignoreocclusion=None, maxage=None):
         """Visualizes the raw camera point clouds on mujin controller
         :param regionname: name of the region
         :param cameranames: a list of camera names to use for visualization, if None, then use all cameras available
         :param pointsize: in meter
+        :param ignoreocclusion: whether to skip occlusion check
+        :param maxage: max time difference in ms allowed between the current time and the timestamp of image used for detection, 0 means infinity
         """
         log.info('sending camera point cloud to mujin controller...')
         if regionname is None:
@@ -192,6 +207,10 @@ class VisionControllerClient(object):
             command['cameranames'] = list(cameranames)
         if pointsize is not None:
             command['pointsize'] = pointsize
+        if ignoreocclusion is not None:
+            command['ignoreocclusion'] = 1 if ignoreocclusion is True else 0
+        if maxage is not None:
+            command['maxage'] = maxage
         response = self._zmqclient.SendCommand(command)
         try:
             log.info('sent point cloud, took %s seconds'%(response['computationtime']/1000.0))
@@ -215,7 +234,7 @@ class VisionControllerClient(object):
     # internal methods
     ############################
 
-    def SaveSnapshot(self,regionname=None, getlatest=None):
+    def SaveSnapshot(self, regionname=None, ignoreocclusion=None, maxage=None):
         """makes each sensor save a snapshot, all files will be saved to the runtime directory of the vision server
         """
         log.info('saving snapshot')
@@ -224,12 +243,14 @@ class VisionControllerClient(object):
         command = {"command": "SaveSnapshot",
                    "regionname": regionname
                    }
-        if getlatest is not None:
-            command['getlatest'] = 1 if getlatest else 0
+        if ignoreocclusion is not None:
+            command['ignoreocclusion'] = 1 if ignoreocclusion is True else 0
+        if maxage is not None:
+            command['maxage'] = maxage
         response = self._zmqclient.SendCommand(command)
         return response
 
-    def UpdateDetectedObjects(self,objects,sendtocontroller=False):
+    def UpdateDetectedObjects(self, objects, sendtocontroller=False):
         """updates the list of objects the vision server maintains
         usage: user may want to process the object location locally and then update the list on the vision server to improve detection
         :param objects: list of dictionaries of object info in world frame, the translation info is in meter, e.g. [{'name':'target_0', 'translation': [1,2,3], 'rotationmat': [[1,0,0],[0,1,0],[0,0,1]], 'score': 0.8}]
