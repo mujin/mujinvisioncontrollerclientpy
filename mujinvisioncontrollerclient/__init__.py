b91f224f48c7c36dc0a88102600edcb5f6e986f5
Huan Liu <liuhuanjim013@gmail.com>
2015-04-08 10:12:20 +0900

    updated VisionControllerClientError

--- mujinvisioncontrollerclient/__init__.py
+++ mujinvisioncontrollerclient/__init__.py
@@ -3,21 +3,24 @@
 
 
 class VisionControllerClientError(Exception):
+    _type = None
+    _desc = None
 
-    def __init__(self, msg=u''):
-        self.msg = unicode(msg)
+    def __init__(self, errortype, errordesc):
+        self._type = errortype
+        self._desc = errordesc
 
     def __unicode__(self):
-        return u'%s: %s' % (self.__class__.__name__, self.msg)
+        return u'%s: %s, %s' % (self.__class__.__name__, self._type, self._desc)
 
     def __str__(self):
         return unicode(self).encode('utf-8')
 
     def __repr__(self):
-        return '<%s(%r)>' % (self.__class__.__name__, self.msg)
+        return '<%s(%s, %s)>' % (self.__class__.__name__, self._type, self._desc)
 
     def __eq__(self, r):
-        return self.msg == r.msg
+        return self._type == r._type and self._desc == r._desc
 
     def __ne__(self, r):
-        return self.msg != r.msg
+        return self._type != r._type or self._desc != r._desc
