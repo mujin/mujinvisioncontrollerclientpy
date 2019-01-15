6b51c396ffc97a17485ce7d45b2b1f0f4062714e
Ziyan <ziyan.zhou@mujin.co.jp>
2019-01-15 11:09:11 +0900

    Enable flake8 checking.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2012-2015 MUJIN Inc
 from distutils.core import setup
+try:
+    from mujincommon.setuptools import Distribution
+except ImportError:
+    from distutils.dist import Distribution
 
 setup(
+    distclass=Distribution,
     name='MujinVisionControllerClient',
     version='0.0.1',
     packages=['mujinvisioncontrollerclient'],
     license='Apache License, Version 2.0',
     long_description=open('README.rst').read(),
+    # flake8 compliance configuration
+    enable_flake8=True,  # Enable checks
+    fail_on_flake=True,  # Fail builds when checks fail
 )
