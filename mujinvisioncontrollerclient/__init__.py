113cbfe823e7244c46a22127d34804979726ebd2
Osbert Ngok <osbert.ngok@mujin.co.jp>
2019-08-16 20:51:52 +0900

    migrated VisionControllerClientError

--- mujinvisioncontrollerclient/__init__.py
+++ mujinvisioncontrollerclient/__init__.py
@@ -1,27 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2014-2015 MUJIN Inc.
+from .version import __version__  # noqa: F401
 
-from .version import __version__ # noqa: F401
-
-class VisionControllerClientError(Exception):
-    _type = None
-    _desc = None
-
-    def __init__(self, errortype, errordesc):
-        self._type = errortype
-        self._desc = errordesc
-
-    def __unicode__(self):
-        return u'%s: %s, %s' % (self.__class__.__name__, self._type, self._desc)
-
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
-    def __repr__(self):
-        return '<%s(%s, %s)>' % (self.__class__.__name__, self._type, self._desc)
-
-    def __eq__(self, r):
-        return self._type == r._type and self._desc == r._desc
-
-    def __ne__(self, r):
-        return self._type != r._type or self._desc != r._desc
+from .visioncontrollerclienterror import VisionControllerClientError
---
+++ mujinvisioncontrollerclient/test_visioncontrollerclienterror.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014-2015 MUJIN Inc.
+
+import six
+import unittest
+from mujinvisioncontrollerclient import visioncontrollerclienterror
+
+make_unicode = None
+
+
+class TestMethods(unittest.TestCase):
+    def test_methods(self):
+        errorType = "错误类型"
+        errorDesc = "错误描述"
+        err = visioncontrollerclienterror.VisionControllerClientError(
+            errortype=errorType, errordesc=errorDesc
+        )
+        if six.PY2:
+            self.assertEqual(
+                str(err), (u"VisionControllerClientError: %s, %s" % (errorType.decode("utf-8"), errorDesc.decode("utf-8"))).encode("utf-8")
+            )
+        else:
+            self.assertEqual(
+                str(err), (u"VisionControllerClientError: %s, %s" % (errorType, errorDesc))
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
